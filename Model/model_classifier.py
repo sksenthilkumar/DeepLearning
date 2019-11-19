8f7dd26a63fc00b4b99f48db6404b34cf7e16562
sksenthilkumar <sksenthilkumar93@gmail.com>
2019-11-19 07:34:02 +0100

    Initial commit for models

--- Model/model_classifier.py
+++ Model/model_classifier.py
@@ -1,23 +1,22 @@
 import torch
+import torchvision.models as models
 
 
-class ModelTemplate(torch.nn.Module):
+class DoubleHeadedClassifier(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.name = None
+        self.name = "DoubleHeadedClassifier"
+        self.feature_extractor = models.resnet50(pretrained=True)
 
-    def forward(self, inter_params=False):
+    def forward(self, input:torch.Tensor, inter_params=False):
         """
         The function to be executed during training the model
+        :param input: input tensor
         :param inter_params: return also the intermediate parameters
         :return:
         """
+        batch_size = input.size(0)
+
         if inter_params:
             raise NotImplementedError
         return None
-
-    def loss(self, ip, target):
-        loss = torch.nn.functional.cross_entropy(ip, target)
-        return loss
-
-
